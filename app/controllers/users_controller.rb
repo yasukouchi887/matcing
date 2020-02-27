e55d568aa9cdb78b603ce316ce8bbd3d50a357ee
yasukouchi <saladoil55@gmail.com>
2020-02-27 00:42:23 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show, :edit, :destroy]
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
